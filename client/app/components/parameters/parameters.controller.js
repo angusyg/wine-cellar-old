08de91839ce9438dae5ec4a449ddcfadac91f7b6
gaelclair <gael.clair@gmail.com>
2017-12-05 16:51:16 +0100

    Modification parameters page

--- client/app/components/parameters/parameters.controller.js
+++ client/app/components/parameters/parameters.controller.js
@@ -8,5 +8,47 @@
 
     function ParametersController() {
         var vm = this;
+
+        vm.selectedParameter = undefined;
+        vm.parameterList = [{
+                name: 'countries',
+                label: 'Pays'
+            },
+            {
+                name: 'regions',
+                label: 'Régions'
+            },
+            {
+                name: 'appellations',
+                label: 'Appellations'
+            },
+            {
+                name: 'origins',
+                label: 'Origines'
+            },
+            {
+                name: 'classifications',
+                label: 'Classements'
+            },
+            {
+                name: 'bottleSizes',
+                label: 'Tailles de bouteille'
+            },
+            {
+                name: 'grapevarieties',
+                label: 'Cépages'
+            },
+            {
+                name: 'types',
+                label: 'Types'
+            },
+        ];
+
+        function init() {
+            vm.selectedParameter = vm.parameterList[0];
+        }
+
+        init();
+
     }
 })(angular);
